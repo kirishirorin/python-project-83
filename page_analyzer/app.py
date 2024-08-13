734a301296c514ad165684ecf317332ed273f9de
Igor <cherkashov270999@gmail.com>
2024-08-13 18:26:17 +0300

    load_env is up

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -11,10 +11,12 @@ import psycopg2
 import requests
 
 
+load_dotenv()
+
+
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
